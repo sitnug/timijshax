7d7e1bb9d03f312121405751f1c965a3fbb8d692
mike <mike@noreply.git.fable.bz>
2026-08-25 04:13:03 +0200

    poop mod britt mutt marie

--- ROGUE/rogue_ui.lua
+++ ROGUE/rogue_ui.lua
@@ -930,7 +930,10 @@ if game.PlaceId == 3541987450 or game.PlaceId == 5208655184 or game.PlaceId == 1
             725659608,
             2557939582,
             1682396718,
-            1525197437
+            1525197437,
+
+
+            7486049096 --brittmarie poop
         },
         aimbot = {
             aimkey_translation = {
